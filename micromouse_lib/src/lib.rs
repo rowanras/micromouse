714e13a853a3a80046db9717ec05dcb1071cfc3c
Tim <timothyhollabaugh@gmail.com>
2019-07-06 15:57:48 -0400

    Config in micromouse_lib

--- micromouse_lib/src/lib.rs
+++ micromouse_lib/src/lib.rs
@@ -1,99 +1,70 @@
-
 #![no_std]
 
-pub mod remote;
+use core::f64;
 
-#[derive(Debug, PartialEq)]
-pub enum Direction {
-    North,
-    South,
-    East,
-    West,
+pub const CONFIG2019: Config = Config {
+    mouse: Mouse {
+        wheel_diameter: 32.0,
+        gearbox_ratio: 75.0,
+        ticks_per_rev: 12.0,
+        wheelbase: 72.0,
+        width: 64.0,
+        length: 88.0,
+        front_offset: 48.0,
+    },
+};
+
+#[derive(Copy, Clone, Debug)]
+pub struct Mouse {
+    pub wheel_diameter: f64,
+    pub gearbox_ratio: f64,
+    pub ticks_per_rev: f64,
+    pub wheelbase: f64,
+    pub width: f64,
+    pub length: f64,
+    pub front_offset: f64,
 }
 
-#[derive(Debug, PartialEq)]
-pub enum AbsoluteMove {
-    North,
-    South,
-    East,
-    West,
-}
+impl Mouse {
+    pub fn ticks_per_mm(&self) -> f64 {
+        (self.ticks_per_rev * self.gearbox_ratio) / (self.wheel_diameter * f64::consts::PI)
+    }
 
-pub fn absolute_to_realative(direction: Direction, absolute_move: AbsoluteMove) -> RelativeMove {
-    match (direction, absolute_move) {
-        (Direction::North, AbsoluteMove::North) => RelativeMove::Forward,
-        (Direction::North, AbsoluteMove::South) => RelativeMove::Backward,
-        (Direction::North, AbsoluteMove::East) => RelativeMove::Right,
-        (Direction::North, AbsoluteMove::West) => RelativeMove::Left,
+    pub fn ticks_to_mm(&self, ticks: f64) -> f64 {
+        ticks / self.ticks_per_mm()
+    }
 
-        (Direction::South, AbsoluteMove::North) => RelativeMove::Backward,
-        (Direction::South, AbsoluteMove::South) => RelativeMove::Forward,
-        (Direction::South, AbsoluteMove::East) => RelativeMove::Left,
-        (Direction::South, AbsoluteMove::West) => RelativeMove::Right,
+    pub fn mm_to_ticks(&self, mm: f64) -> f64 {
+        mm * self.ticks_per_mm()
+    }
 
-        (Direction::East, AbsoluteMove::North) => RelativeMove::Left,
-        (Direction::East, AbsoluteMove::South) => RelativeMove::Right,
-        (Direction::East, AbsoluteMove::East) => RelativeMove::Forward,
-        (Direction::East, AbsoluteMove::West) => RelativeMove::Backward,
+    pub fn ticks_per_rad(&self) -> f64 {
+        self.mm_to_ticks(self.wheelbase / 2.0)
+    }
 
-        (Direction::West, AbsoluteMove::North) => RelativeMove::Right,
-        (Direction::West, AbsoluteMove::South) => RelativeMove::Left,
-        (Direction::West, AbsoluteMove::East) => RelativeMove::Backward,
-        (Direction::West, AbsoluteMove::West) => RelativeMove::Forward,
+    pub fn ticks_to_rads(&self, ticks: f64) -> f64 {
+        ticks / self.ticks_per_rad()
+    }
+
+    pub fn rads_to_ticks(&self, rads: f64) -> f64 {
+        rads * self.ticks_per_rad()
+    }
+
+    pub fn mm_per_rad(&self) -> f64 {
+        self.wheelbase / 2.0
+    }
+
+    pub fn mm_to_rads(&self, mm: f64) -> f64 {
+        mm / self.mm_per_rad()
+    }
+
+    pub fn rads_to_mm(&self, rads: f64) -> f64 {
+        rads * self.mm_per_rad()
     }
 }
 
-#[test]
-fn test_absolute_to_realative() {
-        assert_eq!(absolute_to_realative(Direction::North, AbsoluteMove::North), RelativeMove::Forward);
-        assert_eq!(absolute_to_realative(Direction::North, AbsoluteMove::South), RelativeMove::Backward);
-        assert_eq!(absolute_to_realative(Direction::North, AbsoluteMove::East), RelativeMove::Right);
-        assert_eq!(absolute_to_realative(Direction::North, AbsoluteMove::West), RelativeMove::Left);
-
-        assert_eq!(absolute_to_realative(Direction::South, AbsoluteMove::North), RelativeMove::Backward);
-        assert_eq!(absolute_to_realative(Direction::South, AbsoluteMove::South), RelativeMove::Forward);
-        assert_eq!(absolute_to_realative(Direction::South, AbsoluteMove::East), RelativeMove::Left);
-        assert_eq!(absolute_to_realative(Direction::South, AbsoluteMove::West), RelativeMove::Right);
-
-        assert_eq!(absolute_to_realative(Direction::East, AbsoluteMove::North), RelativeMove::Left);
-        assert_eq!(absolute_to_realative(Direction::East, AbsoluteMove::South), RelativeMove::Right);
-        assert_eq!(absolute_to_realative(Direction::East, AbsoluteMove::East), RelativeMove::Forward);
-        assert_eq!(absolute_to_realative(Direction::East, AbsoluteMove::West), RelativeMove::Backward);
-
-        assert_eq!(absolute_to_realative(Direction::West, AbsoluteMove::North), RelativeMove::Right);
-        assert_eq!(absolute_to_realative(Direction::West, AbsoluteMove::South), RelativeMove::Left);
-        assert_eq!(absolute_to_realative(Direction::West, AbsoluteMove::East), RelativeMove::Backward);
-        assert_eq!(absolute_to_realative(Direction::West, AbsoluteMove::West), RelativeMove::Forward);
+#[derive(Copy, Clone, Debug)]
+pub struct Config {
+    pub mouse: Mouse,
 }
-
-#[derive(Debug, PartialEq)]
-pub enum RelativeMove {
-    Forward,
-    Backward,
-    Left,
-    Right,
-}
-
-pub enum DiagonalEnd {
-    Left45,
-    Left135,
-    Right45,
-    Right135,
-}
-
-pub enum CombinedMove {
-    Straight {cells: u8},
-    Left90,
-    Left180,
-    Right90,
-    Right180,
-    Around,
-    Diagonal {start: DiagonalEnd, cells: u8, end: DiagonalEnd},
-}
-
-pub struct VelocityProfile {
-    
-}
-
-
 
