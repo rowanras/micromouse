cddad9dd75c627f7c1a0b5256e2e60ac8994eb89
Tim <timothyhollabaugh@gmail.com>
2019-07-06 15:58:11 -0400

    simulator: Show mouse position and graph encoder

--- simulation/src/main.rs
+++ simulation/src/main.rs
@@ -1,17 +1,55 @@
+#![recursion_limit = "128"]
+
 extern crate piston_window;
 
-mod maze2;
-mod mouse;
-mod navigate;
+//mod maze2;
+//mod mouse;
+//mod navigate;
 
-use std::fs::File;
+use std::f64;
+
 use std::io::Read;
 
-use piston_window::*;
+use std::time::Duration;
 
-use maze2::Edge;
-use maze2::Maze;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
 
+use piston_window::clear;
+use piston_window::image as draw_image;
+use piston_window::rectangle;
+use piston_window::PistonWindow;
+use piston_window::RenderEvent;
+use piston_window::Texture;
+use piston_window::TextureSettings;
+use piston_window::Transformed;
+use piston_window::WindowSettings;
+
+use image::ImageBuffer;
+
+use plotters::chart::ChartBuilder;
+use plotters::drawing::draw_piston_window;
+use plotters::drawing::BitMapBackend;
+use plotters::drawing::IntoDrawingArea;
+use plotters::series::LineSeries;
+use plotters::style;
+use plotters::style::Color;
+use plotters::style::IntoFont;
+use plotters::style::Palette;
+
+use serialport::DataBits;
+use serialport::FlowControl;
+use serialport::Parity;
+use serialport::SerialPortSettings;
+use serialport::StopBits;
+
+use micromouse_lib::CONFIG2019;
+
+//use maze2::Edge;
+//use maze2::Maze;
+
+/*
 use navigate::Navigate;
 use navigate::CountingDeadEndNavigate;
 use navigate::CountingNavigate;
@@ -26,10 +64,11 @@ use navigate::TwelvePartitionNavigate;
 
 use mouse::Direction;
 use mouse::Mouse;
+*/
 
-pub const CELL_SIZE: f64 = 20.0;
-pub const WALL_SIZE: f64 = 1.0;
+pub const MM_PER_PIXEL: f64 = 10.0;
 
+/*
 pub trait Visualize {
     fn color(&self) -> [f32; 4];
     fn text(&self) -> String;
@@ -109,175 +148,198 @@ fn draw_maze<C: Visualize + Copy>(
 
     drawings
 }
+*/
+
+#[derive(Debug)]
+struct MouseState {
+    time: f64,
+    left: f64,
+    right: f64,
+    x: f64,
+    y: f64,
+    dir: f64,
+}
 
 fn main() {
-    //let maze = Maze::new((), Edge::Open);
+    let config = CONFIG2019;
 
-    let mut args = std::env::args().skip(1);
+    let states: Arc<Mutex<Vec<MouseState>>> = Arc::new(Mutex::new(Vec::new()));
 
-    let maze_path = args.next().unwrap_or("mouse_maze_tool/mazefiles/binary/APEC2016.MAZ".to_owned());
+    let state_uart = Arc::clone(&states);
+    thread::spawn(move || {
+        let serial_settings = SerialPortSettings {
+            baud_rate: 9600,
+            data_bits: DataBits::Eight,
+            flow_control: FlowControl::None,
+            parity: Parity::None,
+            stop_bits: StopBits::One,
+            timeout: Duration::from_secs(1),
+        };
 
-    println!("Loading from: {}", maze_path);
+        let mut port =
+            serialport::open_with_settings("/dev/ttyUSB0", &serial_settings)
+                .expect("Could not open port");
 
-    let mut file =
-        File::open(maze_path)
-            .unwrap();
-    let mut bytes = [0; 256];
-    file.read_exact(&mut bytes).unwrap();
-    let maze = Maze::from_file(0, bytes);
+        port.write_all(&[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+            .expect("Could not write bytes");
 
-    let nav_string = args.next().unwrap_or("TwelvePartitionNavigate".to_owned());
+        let mut leftover_bytes = Vec::new();
+        port.read_to_end(&mut leftover_bytes);
 
-    let nav: Box<dyn Navigate<Cell = u8>> = match nav_string.as_ref() {
-        "CountingNavigate" => Box::new(CountingNavigate::new()),
-        "CountingDeadEndNavigate" => Box::new(CountingDeadEndNavigate::new()),
-        "FloodFillNavigate" => Box::new(FloodFillNavigate::new()),
-        "FloodFillSquareNavigate" => Box::new(FloodFillSquareNavigate::new()),
-        "FloodFillDeadEndNavigate" => Box::new(FloodFillDeadEndNavigate::new()),
-        "FloodFillSquareDeadEndNavigate" => Box::new(FloodFillSquareDeadEndNavigate::new()),
-        "TwelvePartitionNavigate" => Box::new(TwelvePartitionNavigate::new()),
-        _ => Box::new(TwelvePartitionNavigate::new()),
-    };
+        port.write_all(&[0x01, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00])
+            .expect("Could not write bytes");
 
-    //let nav= LeftWall::new();
-    //let nav = DeadEndNavigate::new();
-    //let nav = RandomNavigate::new([0; 16]);
-    //let nav = CountingNavigate::new();
-    //let nav = CountingDeadEndNavigate::new();
-    //let nav = FloodFillNavigate::new();
-    //let nav = FloodFillSquareDeadEndNavigate::new();
-    //let nav = TwelvePartitionNavigate::new();
-    let mut mouse = Mouse::new(nav, maze);
+        let mut buffer = [0; 8];
 
-    let start_time = std::time::Instant::now();
+        loop {
+            port.read_exact(&mut buffer)
+                .expect("Could not read from port");
 
-    /*
-    loop {
-        let (x, y, d) = mouse.maze_location();
+            let time = (((buffer[3] as u32) << 16)
+                | ((buffer[2] as u32) << 8)
+                | (buffer[1] as u32)) as f64
+                / 1000.0;
+            let left = config.mouse.ticks_to_mm(
+                (((buffer[5] as i32) << 8) | (buffer[4] as i32)) as f64,
+            );
+            let right = config.mouse.ticks_to_mm(
+                (((buffer[7] as i32) << 8) | (buffer[6] as i32)) as f64,
+            );
 
-        let runtime = std::time::Instant::now().duration_since(start_time);
+            let mut s = state_uart.lock().unwrap();
 
-        if x >= 7 && x <= 8 && y >= 7 && y <= 8 || runtime > std::time::Duration::from_secs(10) {
-            println!("Won!");
+            let (x, y, dir) = if let Some(last_state) = s.last() {
+                let delta_left = left - last_state.left;
+                let delta_right = right - last_state.right;
 
-            let maze = mouse.maze();
+                let delta_linear = (delta_left + delta_right) / 2.0;
+                let delta_angular =
+                    config.mouse.mm_to_rads((delta_left - delta_right) / 2.0);
 
-            for y in 0..maze2::HEIGHT {
-                for x in 0..maze2::WIDTH {
-                    let (cell, north_edge, south_edge, east_edge, west_edge) =
-                        maze.get(x, (maze2::HEIGHT - 1) - y);
+                let mid_dir = last_state.dir + delta_angular / 2.0;
 
-                    print!("{}", cell.text());
+                (
+                    last_state.x + delta_linear * f64::cos(mid_dir),
+                    last_state.y + delta_linear * f64::sin(mid_dir),
+                    last_state.dir + delta_angular,
+                )
+            } else {
+                (90.0, 90.0, f64::consts::PI / 2.0)
+            };
 
-                    if x != maze2::WIDTH - 1 {
-                        print!("\t");
-                    } else {
-                        print!("\n");
-                    }
-                }
-            }
+            let new_state = MouseState {
+                time,
+                left,
+                right,
+                x,
+                y,
+                dir,
+            };
 
-            break;
+            s.push(new_state);
         }
+    });
 
-        mouse.run(1.0/60.0);
-    }
-    */
+    let states_maze = Arc::clone(&states);
 
-    let mut window: PistonWindow = WindowSettings::new(
-        nav_string,
-        [16 * CELL_SIZE as u32, 16 * CELL_SIZE as u32],
-    )
-    .exit_on_esc(true)
-    .build()
-    .unwrap();
+    let mut window: PistonWindow = WindowSettings::new("Mouse", [800, 300])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
 
-    window.set_ups(60);
-    window.set_max_fps(20);
-
-    let font = "/usr/share/fonts/TTF/FiraMono-Regular.ttf";
-    let factory = window.factory.clone();
-    let mut glyphs =
-        Glyphs::new(font, factory, TextureSettings::new()).unwrap();
+    let mut texture_context = window.create_texture_context();
 
     while let Some(event) = window.next() {
-        if let Some(u) = event.update_args() {
-
-            let (x, y, d) = mouse.maze_location();
-
-            if x >= 7 && x <= 8 && y >= 7 && y <= 8 {
-                println!("Won!");
-
-                let maze = mouse.maze();
-
-                for y in 0..maze2::HEIGHT {
-                    for x in 0..maze2::WIDTH {
-                        let (cell, north_edge, south_edge, east_edge, west_edge) =
-                            maze.get(x, (maze2::HEIGHT - 1) - y);
-
-                        print!("{}", cell.text());
-
-                        if x != maze2::WIDTH - 1 {
-                            print!("\t");
-                        } else {
-                            print!("\n");
-                        }
-                    }
-                }
-
-                //window.set_should_close(true);
-            }
-
-            //mouse.run(u.dt);
-        }
-
-        if let Some(r) = event.render_args() {
-            window.draw_2d(&event, |context, graphics| {
+        if let Some(_) = event.render_args() {
+            window.draw_2d(&event, |context, graphics, _device| {
                 clear([1.0; 4], graphics);
 
-                let drawings = draw_maze(mouse.maze());
+                let states = states_maze.lock().unwrap();
 
-                for ((string, x, y), rects) in drawings {
-                    for (color, rect) in rects {
-                        rectangle(color, rect, context.transform, graphics);
-                    }
-                    let transform = context.transform.trans(x, y);
-                    /*
-                    text(
-                        [0.0, 0.0, 0.0, 1.0],
-                        10,
-                        &string,
-                        &mut glyphs,
+                if let Some(state) = dbg!(states.last()) {
+                    let transform = context
+                        .transform
+                        .trans(state.x / MM_PER_PIXEL, state.y / MM_PER_PIXEL)
+                        .rot_rad(state.dir);
+
+                    rectangle(
+                        [0.0, 1.0, 0.0, 1.0],
+                        [
+                            -(config.mouse.length - config.mouse.front_offset)
+                                / MM_PER_PIXEL,
+                            -config.mouse.width / MM_PER_PIXEL / 2.0,
+                            config.mouse.length / MM_PER_PIXEL,
+                            config.mouse.width / MM_PER_PIXEL,
+                        ],
                         transform,
                         graphics,
-                    )
-                    .unwrap();
-                    */
+                    );
                 }
 
-                let (mouse_x, mouse_y, mouse_dir) = mouse.world_location();
+                let mut graph_image_buf = Vec::new();
 
-                let transform = context
-                    .transform
-                    .trans(
-                        CELL_SIZE / 2.0 + mouse_x,
-                        CELL_SIZE / 2.0
-                            + (maze2::HEIGHT - 1) as f64 * CELL_SIZE
-                            - mouse_y,
+                {
+                    let root = BitMapBackend::with_buffer(
+                        &mut graph_image_buf,
+                        (400, 300),
                     )
-                    .rot_deg(mouse_dir);
+                    .into_drawing_area();
 
-                rectangle(
-                    [0.0, 1.0, 0.0, 1.0],
-                    [
-                        -mouse::WIDTH / 2.0,
-                        -mouse::LENGTH / 2.0,
-                        mouse::WIDTH,
-                        mouse::LENGTH,
-                    ],
-                    transform,
-                    graphics,
-                );
+                    root.fill(&style::White).unwrap();
+
+                    let time = if let Some(state) = states.last() {
+                        state.time
+                    } else {
+                        0.0
+                    };
+
+                    let mut cc = ChartBuilder::on(&root)
+                        .margin(10)
+                        .caption("Left Encoder", ("Arial", 20).into_font())
+                        .x_label_area_size(40)
+                        .y_label_area_size(50)
+                        .build_ranged(time - 60.0..time, 0.0..50.0)
+                        .unwrap();
+
+                    cc.configure_mesh()
+                        .x_label_formatter(&|x| format!("{}", x))
+                        .y_label_formatter(&|y| format!("{}", y))
+                        .x_labels(15)
+                        .y_labels(10)
+                        .x_desc("seconds")
+                        .y_desc("mm")
+                        .axis_desc_style(("Arial", 15).into_font())
+                        .disable_x_mesh()
+                        .disable_y_mesh()
+                        .draw()
+                        .unwrap();
+
+                    cc.draw_series(LineSeries::new(
+                        states.iter().map(|s| (s.time, s.left)),
+                        &style::Palette99::pick(0),
+                    ))
+                    .unwrap();
+                }
+
+                // Add the alpha channel
+                let graph_image_buf: Vec<u8> = graph_image_buf
+                    .chunks_exact(3)
+                    .flat_map(|rgb| {
+                        vec![rgb[0], rgb[1], rgb[2], 255].into_iter()
+                    })
+                    .collect();
+
+                let graph_texture = Texture::from_image(
+                    &mut texture_context,
+                    &ImageBuffer::from_vec(400, 300, graph_image_buf.clone())
+                        .expect("Buffer not big enough!"),
+                    &TextureSettings::new(),
+                )
+                .unwrap();
+
+                let transform = context.transform.trans(400.0, 0.0);
+
+                draw_image(&graph_texture, transform, graphics);
             });
         }
     }
