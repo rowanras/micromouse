51c25ba349ad2a9a1ac936d7d0a85cbc6aff0c70
Tim <timothyhollabaugh@gmail.com>
2019-08-03 12:59:05 -0400

    simulation: Don't use property trait

--- simulation/src/gui.rs
+++ simulation/src/gui.rs
@@ -1,7 +1,6 @@
 use std::borrow::Cow;
 use std::collections::HashMap;
 use std::collections::HashSet;
-use std::fmt::Display;
 use std::rc::Rc;
 use std::sync::mpsc::Sender;
 use std::sync::Arc;
@@ -15,7 +14,7 @@ use glium::glutin::Event;
 use glium::glutin::WindowEvent;
 use glium::texture::ClientFormat;
 use glium::texture::RawImage2d;
-use glium::Display as GlDisplay;
+use glium::Display;
 use glium::Rect;
 use glium::Surface;
 use glium::Texture2d;
@@ -39,75 +38,71 @@ use imgui_winit_support::WinitPlatform;
 
 use arrayvec::ArrayVec;
 
-use micromouse_lib::control::Target;
 use micromouse_lib::mouse::Mouse;
 use micromouse_lib::msgs::Msg as MouseMsg;
 use micromouse_lib::msgs::MsgId as MouseMsgId;
 
 use crate::SimulatorState;
 
-fn init_properties() -> Vec<Box<dyn Property>> {
-    vec![
-        Box::new(SimpleProperty::new(
-            "Time",
-            |m| m.time,
+macro_rules! ui_f32 {
+    ($m:ident, $f:expr, $s:expr, $l:expr, $u:expr, $c:expr) => {
+        $c.view_float(
+            $u,
+            $s,
+            $l,
+            ImString::new(stringify!($m)).as_ref(),
+            $f,
             None,
-            Some(MouseMsgId::Time),
-        )),
-        Box::new(SimpleProperty::new(
-            "Left Position",
-            |m| m.left_pos,
+            Some(MouseMsgId::$m),
+        );
+    }
+}
+
+macro_rules! ui_f32c {
+    ($m:ident, $f:expr, $s:expr, $l:expr, $u:expr, $c:expr) => {
+        $c.view_float(
+            $u,
+            $s,
+            $l,
+            ImString::new(stringify!($m)).as_ref(),
+            $f,
             None,
-            Some(MouseMsgId::LeftPos),
-        )),
-        Box::new(SimpleProperty::new(
-            "Right Position",
-            |m| m.right_pos,
-            None,
-            Some(MouseMsgId::RightPos),
-        )),
-        Box::new(SimpleProperty::new(
-            "Linear Position",
-            |m| m.linear_pos,
-            None,
-            Some(MouseMsgId::LinearPos),
-        )),
-        Box::new(SimpleProperty::new(
-            "Angular Position",
-            |m| m.angular_pos,
-            None,
-            Some(MouseMsgId::AngularPos),
-        )),
-        Box::new(SimpleProperty::new(
-            "Linear Power",
-            |m| m.linear_power,
-            Some(MouseMsg::LinearPower),
-            Some(MouseMsgId::LinearPower),
-        )),
-        Box::new(SimpleProperty::new(
-            "Angular Power",
-            |m| m.angular_power,
-            Some(MouseMsg::AngularPower),
-            Some(MouseMsgId::AngularPower),
-        )),
-        Box::new(SimpleProperty::new(
-            "Left Power",
-            |m| m.left_power,
-            None,
-            Some(MouseMsgId::LeftPower),
-        )),
-        Box::new(SimpleProperty::new(
-            "Right Power",
-            |m| m.right_power,
-            None,
-            Some(MouseMsgId::RightPower),
-        )),
-    ]
+            None
+        );
+    }
+}
+
+macro_rules! ui_f32m {
+    ($m:ident, $f:expr, $s:expr, $l:expr, $u:expr, $c:expr) => {
+        $c.view_float(
+            $u,
+            $s,
+            $l,
+            ImString::new(stringify!($m)).as_ref(),
+            $f,
+            Some(MouseMsg::$m),
+            Some(MouseMsgId::$m),
+        );
+    }
+}
+
+macro_rules! ui_u8m {
+    ($m:ident, $f:expr, $s:expr, $l:expr, $u:expr, $c:expr) => {
+        $c.view_float(
+            $u,
+            $s,
+            $l,
+            ImString::new(stringify!($m)).as_ref(),
+            |m: &Mouse, lm: Option<&Mouse>| ($f)(m, lm) as f32,
+            Some(|v| MouseMsg::$m(v as u8)),
+            Some(MouseMsgId::$m),
+        );
+    }
 }
 
 #[derive(Debug)]
 pub enum GuiMsg {
-    Mouse(MouseMsg),
+    Mouse(MouseMsg)
 }
 
 struct Plot {
@@ -122,109 +117,6 @@ impl std::fmt::Debug for Plot {
     }
 }
 
-trait Property: std::fmt::Debug {
-    fn render(&mut self, ui: &Ui, mouse: &Mouse) -> Option<MouseMsg>;
-    fn plot(&self) -> Option<Box<Fn(&Mouse) -> f32>>;
-    fn log(&self) -> Option<MouseMsgId>;
-}
-
-struct SimpleProperty<T: Default> {
-    name: String,
-    update_fn: fn(&Mouse) -> T,
-    update_msg: Option<fn(T) -> MouseMsg>,
-    log_msg: Option<MouseMsgId>,
-    editted: T,
-    plot: bool,
-    log: bool,
-}
-
-impl<T: Default> std::fmt::Debug for SimpleProperty<T> {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "SimpleProperty {{ name: {}, plot: {}, log: {} }}", self.name, self.plot, self.log)
-    }
-}
-
-impl<T: Default> SimpleProperty<T> {
-    pub fn new(
-        name: &str,
-        update_fn: fn(&Mouse) -> T,
-        update_msg: Option<fn(T) -> MouseMsg>,
-        log_msg: Option<MouseMsgId>,
-    ) -> SimpleProperty<T> {
-        SimpleProperty {
-            name: name.to_owned(),
-            update_fn,
-            update_msg,
-            log_msg,
-            editted: Default::default(),
-            log: false,
-            plot: false,
-        }
-    }
-}
-
-impl Property for SimpleProperty<f32> {
-    fn render(&mut self, ui: &Ui, mouse: &Mouse) -> Option<MouseMsg> {
-        ui.push_id(&self.name);
-        let popup_id = ImString::new(format!("Popup_{}", self.name));
-
-        ui.columns(2, im_str!("state"), true);
-        ui.text(&format!("{}: {}", self.name, (self.update_fn)(mouse)));
-
-        ui.next_column();
-
-        ui.checkbox(im_str!("plot"), &mut self.plot);
-
-        ui.same_line(0.0);
-
-        if let Some(msg) = self.log_msg {
-            let update_log = ui.checkbox(im_str!("log"), &mut self.log);
-        }
-
-        let edit = if let Some(msg) = self.update_msg {
-            ui.same_line(0.0);
-            {
-                let _w = ui.push_item_width(-40.0);
-                ui.input_float(im_str!(""), &mut (self.editted.into()))
-                    .build();
-            }
-            ui.same_line(0.0);
-            {
-                let _w = ui.push_item_width(-1.0);
-                if ui.small_button(im_str!("Set")) {
-                    Some(msg(self.editted))
-                } else {
-                    None
-                }
-            }
-        } else {
-            None
-        };
-
-        ui.next_column();
-        ui.pop_id();
-
-        edit
-    }
-
-    fn plot(&self) -> Option<Box<Fn(&Mouse) -> f32>> {
-        if self.plot {
-            let update_fn = self.update_fn;
-            Some(Box::new(move |m| (update_fn)(m)))
-        } else {
-            None
-        }
-    }
-
-    fn log(&self) -> Option<MouseMsgId> {
-        if let (Some(msg), true) = (self.log_msg, self.log) {
-            Some(msg)
-        } else {
-            None
-        }
-    }
-}
-
 #[derive(Debug)]
 struct State {
     pub plots: HashMap<String, Plot>,
@@ -233,7 +125,6 @@ struct State {
     pub history_active: bool,
     pub editables: HashMap<String, f32>,
     pub logged: HashSet<MouseMsgId>,
-    pub properties: Vec<Box<dyn Property>>,
 }
 
 impl State {
@@ -245,7 +136,6 @@ impl State {
             history_active: false,
             editables: HashMap::new(),
             logged: HashSet::new(),
-            properties: init_properties(),
         }
     }
 }
@@ -270,7 +160,7 @@ impl<Msg: 'static + Send> Gui<Msg> {
         ui.push_id(label);
         let popup_id = ImString::new(format!("Popup_{}", label));
 
-        ui.columns(2, im_str!("state"), true);
+        ui.columns(2, im_str!("state"), false);
         ui.text(&format!("{}: {}", label, value(mouse, last_mouse)));
 
         ui.next_column();
@@ -284,24 +174,22 @@ impl<Msg: 'static + Send> Gui<Msg> {
                 Plot {
                     function: value,
                     showing: false,
-                    range: [200.0, 0.0],
+                    range: [200.0, 0.0]
                 },
             );
         }
 
+        ui.same_line(0.0);
+
         if let Some(id) = id {
             let mut log = self.state.logged.remove(&id);
-            ui.same_line(0.0);
             let update_log = ui.checkbox(im_str!("log"), &mut log);
             if log {
                 self.state.logged.insert(id);
             }
             if update_log {
                 let log_msg_ids = self.state.logged.iter().cloned().collect();
-                self.tx
-                    .send((self.on_gui_msg)(GuiMsg::Mouse(MouseMsg::Logged(
-                        log_msg_ids,
-                    ))));
+                self.tx.send((self.on_gui_msg)(GuiMsg::Mouse(MouseMsg::Logged(log_msg_ids))));
             }
         }
 
@@ -317,14 +205,14 @@ impl<Msg: 'static + Send> Gui<Msg> {
                 {
                     let _w = ui.push_item_width(-1.0);
                     if ui.small_button(im_str!("Set")) {
-                        self.tx
-                            .send((self.on_gui_msg)(GuiMsg::Mouse(on_edit(*editable))));
+                        self.tx.send((self.on_gui_msg)(GuiMsg::Mouse(on_edit(*editable as f32))));
                     }
                 }
             } else {
-                self.state
-                    .editables
-                    .insert(key.to_string(), value(mouse, last_mouse));
+                self.state.editables.insert(
+                    key.to_string(),
+                    value(mouse, last_mouse),
+                );
             }
         }
 
@@ -334,29 +222,53 @@ impl<Msg: 'static + Send> Gui<Msg> {
     }
 
     fn view_state(&mut self, ui: &mut Ui, m: &Mouse, lm: Option<&Mouse>) {
-        let mut send_log = false;
-        let mut all_log = ArrayVec::new();
-        for property in self.state.properties.iter_mut() {
-            let previous_log_msg = property.log();
-            property.render(ui, m);
-            let next_log_msg = property.log();
-            if let Some(msg) = next_log_msg {
-                all_log.push(msg)
-            }
-            if previous_log_msg != next_log_msg {
-                send_log = true;
-            }
-        }
 
-        if send_log {
-            self.tx.send((self.on_gui_msg)(GuiMsg::Mouse(MouseMsg::Logged(all_log))));
-        }
+        ui_f32!(Time, |m, _| m.time, m, lm, ui, self);
+        ui_f32c!(DeltaTime, |m, lm| lm.map(|lm| m.time - lm.time).unwrap_or(0.0), m, lm, ui, self);
+
+        ui_f32m!(LeftPos, |m, _| m.left_pos, m, lm, ui, self);
+        ui_f32m!(RightPos, |m, _| m.right_pos, m, lm, ui, self);
+        ui_f32c!(LeftVelocity, |m, lm| {
+            lm.map(|lm| (m.left_pos - lm.left_pos) / (m.time - lm.time)).unwrap_or(0.0)
+        }, m, lm, ui, self);
+        ui_f32c!(RightVelocity, |m, lm| {
+            lm.map(|lm| (m.right_pos - lm.right_pos) / (m.time - lm.time)).unwrap_or(0.0)
+        }, m, lm, ui, self);
+        ui_f32m!(LeftPower, |m, _| m.left_power, m, lm, ui, self);
+        ui_f32m!(RightPower, |m, _| m.right_power, m, lm, ui, self);
+        ui_f32m!(Battery, |m, _| m.battery, m, lm, ui, self);
+        ui_u8m!(LeftDistance, |m: &Mouse, _| m.left_distance, m, lm, ui, self);
+        ui_u8m!(FrontDistance, |m: &Mouse, _| m.front_distance, m, lm, ui, self);
+        ui_u8m!(RightDistance, |m: &Mouse, _| m.right_distance, m, lm, ui, self);
+
+        // Calculated
+        ui_f32m!(LinearPos, |m, _| m.linear_pos, m, lm, ui, self);
+        ui_f32m!(AngularPos, |m, _| m.angular_pos, m, lm, ui, self);
+        ui_f32c!(LinearVelocity, |m, lm| {
+            lm.map(|lm| (m.linear_pos - lm.linear_pos) / (m.time - lm.time)).unwrap_or(0.0)
+        }, m, lm, ui, self);
+        ui_f32c!(AngularVelocity, |m, lm| {
+            lm.map(|lm| (m.angular_pos - lm.angular_pos) / (m.time - lm.time)).unwrap_or(0.0)
+        }, m, lm, ui, self);
+        ui_f32m!(LinearPower, |m, _| m.linear_power, m, lm, ui, self);
+        ui_f32m!(AngularPower, |m, _| m.angular_power, m, lm, ui, self);
+        ui_f32m!(LinearSet, |m, _| m.linear_control.position as f32, m, lm, ui, self);
+        ui_f32m!(AngularSet, |m, _| m.angular_control.position as f32, m, lm, ui, self);
+
+        // Config
+        ui_f32m!(LinearP, |m, _| m.linear_control.pid.p_gain as f32, m, lm, ui, self);
+        ui_f32m!(LinearI, |m, _| m.linear_control.pid.i_gain as f32, m, lm, ui, self);
+        ui_f32m!(LinearD, |m, _| m.linear_control.pid.d_gain as f32, m, lm, ui, self);
+        ui_f32m!(LinearAcc, |m, _| m.linear_control.acceleration as f32, m, lm, ui, self);
+        ui_f32m!(AngularP, |m, _| m.angular_control.pid.p_gain as f32, m, lm, ui, self);
+        ui_f32m!(AngularI, |m, _| m.angular_control.pid.i_gain as f32, m, lm, ui, self);
+        ui_f32m!(AngularD, |m, _| m.angular_control.pid.d_gain as f32, m, lm, ui, self);
+        ui_f32m!(AngularAcc, |m, _| m.angular_control.acceleration as f32, m, lm, ui, self);
     }
 
     fn run_ui(&mut self, ui: &mut Ui, simulator_state: &SimulatorState) {
         let mice: Vec<&Mouse> = if self.state.history_active {
-            simulator_state
-                .mice
+            simulator_state.mice
                 .iter()
                 .filter(|m| (m.time as f32) < self.state.history_time)
                 .collect()
@@ -374,23 +286,25 @@ impl<Msg: 'static + Send> Gui<Msg> {
             .size([400.0, 800.0], Condition::FirstUseEver)
             .position([400.0, 0.0], Condition::FirstUseEver)
             .build(|| {
-                ui.checkbox(im_str!("Show history"), &mut self.state.history_active);
-                ui.input_float(im_str!("Historic time"), &mut self.state.history_time)
-                    .build();
+                ui.checkbox(
+                    im_str!("Show history"),
+                    &mut self.state.history_active,
+                );
+                ui.input_float(
+                    im_str!("Historic time"),
+                    &mut self.state.history_time,
+                )
+                .build();
 
                 if let Some(mouse) = mice.last() {
-                    self.view_state(
-                        ui,
-                        mouse,
-                        mice.get((mice.len() as i32 - 2).max(0) as usize)
-                            .map(|&m| m),
-                    );
+                    self.view_state(ui, mouse, mice.get((mice.len() as i32 - 2).max(0) as usize).map(|&m| m));
                 } else {
                     ui.text("No state yet :(");
                 }
             });
 
-        let count_plots = self.state.plots.iter().filter(|(l, p)| p.showing).count();
+        let count_plots =
+            self.state.plots.iter().filter(|(l, p)| p.showing).count();
         if count_plots > 0 {
             ui.window(im_str!("Plots"))
                 .size(
@@ -403,20 +317,23 @@ impl<Msg: 'static + Send> Gui<Msg> {
 
                     ui.input_float(im_str!("plot range"), range).build();
 
-                    for (label, mut plot) in self.state.plots.iter_mut().filter(|(l, p)| p.showing)
+                    for (label, mut plot) in
+                        self.state.plots.iter_mut().filter(|(l, p)| p.showing)
                     {
-                        let last = mice.last().map(|m| m.time).unwrap_or(0.0);
+                        let last =
+                            mice.last().map(|m| m.time).unwrap_or(0.0);
 
                         let points: Vec<_> = mice
                             .iter()
-                            .filter(|m| m.time as f32 > last as f32 - *range)
-                            .collect();
+                            .filter(|m| m.time as f32 > last as f32 - *range).collect();
 
                         let points: Vec<_> = points
                             .first()
                             .iter()
                             .map(|s| (plot.function)(s, None))
-                            .chain(points.windows(2).map(|s| (plot.function)(s[1], Some(s[0]))))
+                            .chain(points.windows(2)
+                                .map(|s| (plot.function)(s[1], Some(s[0])))
+                            )
                             .collect();
 
                         ui.push_id(label);
@@ -475,10 +392,7 @@ impl<Msg: 'static + Send> Gui<Msg> {
         ui.window(im_str!("Simulator"))
             .size([400.0, 300.0], Condition::FirstUseEver)
             .build(|| {
-                ui.label_text(
-                    im_str!("Uart buffer"),
-                    ImString::new(simulator_state.uart_buffer_len.to_string()).as_ref(),
-                );
+                ui.label_text(im_str!("Uart buffer"), ImString::new(simulator_state.uart_buffer_len.to_string()).as_ref());
             });
     }
 }
@@ -495,8 +409,8 @@ pub fn start<Msg: 'static + Send>(
         let builder = glutin::WindowBuilder::new()
             .with_title(title.to_owned())
             .with_dimensions(glutin::dpi::LogicalSize::new(1024f64, 768f64));
-        let display =
-            GlDisplay::new(builder, context, &events_loop).expect("Failed to initialize display");
+        let display = Display::new(builder, context, &events_loop)
+            .expect("Failed to initialize display");
 
         let mut imgui = Context::create();
         imgui.set_ini_filename(None);
@@ -529,14 +443,10 @@ pub fn start<Msg: 'static + Send>(
 
         imgui.io_mut().font_global_scale = (1.0 / hidpi_factor) as f32;
 
-        let mut renderer =
-            GliumRenderer::init(&mut imgui, &display).expect("Failed to initialize renderer");
+        let mut renderer = GliumRenderer::init(&mut imgui, &display)
+            .expect("Failed to initialize renderer");
 
-        let mut gui = Gui {
-            tx,
-            on_gui_msg,
-            state: State::new(),
-        };
+        let mut gui = Gui { tx, on_gui_msg, state: State::new() };
 
         let gl_window = display.gl_window();
         let window = gl_window.window();
